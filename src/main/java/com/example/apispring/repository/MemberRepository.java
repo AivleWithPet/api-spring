92ec681166000044b632e2d193d27b3d4e6df336
Taeho <ice_98@naver.com>
2023-05-17 20:49:11 +0900

    #3 - test1

--- src/main/java/com/example/apispring/repository/MemberRepository.java
+++ src/main/java/com/example/apispring/repository/MemberRepository.java
@@ -9,7 +9,7 @@ import java.util.Map;
 @Repository
 public class MemberRepository {
     private static final Map<String, Member> store = new HashMap<>();
-//test
+//test1
     public Member save(Member member) {
         store.put(member.getUserid(), member);
         return member;
