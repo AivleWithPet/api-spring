0e8ea8b0fdf7cd76ecabcc21915e016da19f4f54
minkycho0412 <minkycho0412@gmail.com>
2023-08-10 23:13:03 +0900

    FEAT:image Response

--- src/main/java/com/example/apispring/dto/ResultResponseDto.java
+++ src/main/java/com/example/apispring/dto/ResultResponseDto.java
@@ -4,11 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class ResultResponseDto {
     private String inform;
     private String supplements;
     private String diseaseName;
-
+    private LocalDateTime createdAt;
+    private String imageBase64;
 }
